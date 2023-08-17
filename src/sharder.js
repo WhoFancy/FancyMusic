7d37d5ca3cfd81acb8cb4d13fd8a7320af65e57f
WhoFancy <142501135+WhoFancy@users.noreply.github.com>
2023-08-17 19:21:26 +0700

    Add files via upload

---
+++ src/sharder.js
@@ -0,0 +1,13 @@
+const { ClusterManager } = require("discord-hybrid-sharding");
+require("dotenv").config();
+
+const manager = new ClusterManager(`${__dirname}/index.js`, {
+    totalShards: "auto",
+    shardsPerClusters: 2,
+    totalClusters: "auto",
+    mode: "process",
+    token: process.env.TOKEN || "YOUR_BOT_TOKEN", // <=== PASTE YOU TOKEN HERE
+});
+
+manager.on("clusterCreate", (cluster) => console.log(`[INFO] Fancy Launched Cluster ${cluster.id}`));
+manager.spawn({ timeout: -1 });
