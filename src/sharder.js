02f2a5d3208b9a490a6cdb13126c7b4989a6f842
Fancy <142501135+WhoFancy@users.noreply.github.com>
2023-08-18 11:42:02 +0700

    Update sharder.js

--- src/sharder.js
+++ src/sharder.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const manager = new ClusterManager(`${__dirname}/index.js`, {
     totalShards: "auto",
-    shardsPerClusters: 2,
+    shardsPerClusters: 5,
     totalClusters: "auto",
     mode: "process",
     token: process.env.TOKEN || "YOUR_BOT_TOKEN", // <=== PASTE YOU TOKEN BOT HERE
