87e82a446cc3ecabc7d543ba19aa81648240c3a0
Fancy <142501135+WhoFancy@users.noreply.github.com>
2023-08-22 18:49:27 +0700

    Update FancyUptime.js

--- src/commands/Slash/Information/FancyUptime.js
+++ src/commands/Slash/Information/FancyUptime.js
@@ -1,4 +1,5 @@
 const { EmbedBuilder } = require("discord.js");
+const os = require("os");
 const pretty = require('pretty-ms');
 
 module.exports = {
@@ -19,11 +20,21 @@ module.exports = {
         premium: false,
     },
     run: async (client, interaction) => {
-        await interaction.deferReply({ ephemeral: false });
+    let uptime = await os.uptime();
+
+        let d = Math.floor(uptime / (3600 * 24));
+        let h = Math.floor((uptime % (3600 * 24)) / 3600);
+        let m = Math.floor((uptime % 3600) / 60);
+        let s = Math.floor(uptime % 60);
+        let dDisplay = d > 0 ? d + (d === 1 ? " day, " : " days, ") : "";
+        let hDisplay = h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : "";
+        let mDisplay = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "";
+        let sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
+      await interaction.deferReply({ ephemeral: false });
 
         const embed = new EmbedBuilder()
             .setTitle(`\<:fancy:1135883644649611264> Uptime Information!`)
-            .setDescription(`\`\`\`yml\n❓ Status : Online\n⏲ Uptime : ${pretty(client.uptime)}\n\`\`\``)
+            .setDescription(`\`\`\`yml\n❓ Status : Online\n⏲ Uptime : ${pretty(client.uptime)}\n📊 System Uptime : ${dDisplay + hDisplay + mDisplay + sDisplay}\n\`\`\``)
             .setColor(client.color);
 
         interaction.followUp({ embeds: [embed] });
